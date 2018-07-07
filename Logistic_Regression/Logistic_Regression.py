68fca6488e05a77d78ebd630126d4838ab8b8718
Santosh Shukla <santosh.shukla1992@gmail.com>
2018-07-07 21:24:05 +0530

    This version of code gave 74.6% Accuracy on Kaggle website.

--- Logistic_Regression/Logistic_Regression.py
+++ Logistic_Regression/Logistic_Regression.py
@@ -33,7 +33,7 @@ model.add(Dense(1,activation= 'sigmoid'))
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 #### Train the model
-model.fit(input_NN,output_NN, epochs=100, batch_size=16)
+model.fit(input_NN,output_NN, epochs=1000, batch_size=16)
 
 #### Evaluate the model
 scores = model.evaluate(input_NN,output_NN)
@@ -41,15 +41,22 @@ print("\n%s: %.2f%%" % (model.metrics_names[1], scores[1]*100))
 
 #### Importing test dataset
 df = pd.read_csv("Dataset/test.csv")
-df1 = pd.read_csv("Dataset/gender_submission.csv")
-df = pd.merge(df1, df)
-df = df.drop(['Name','Ticket','Cabin','PassengerId'], axis=1)
-df = df.dropna()
+#df1 = pd.read_csv("Dataset/gender_submission.csv")
+#df = pd.merge(df1, df)
+df = df.drop(['Name','Ticket','Cabin'], axis=1)
+#df = df.dropna()
+df1 = pd.DataFrame()
+df1['PassengerId'] = df['PassengerId'].copy()
+df = df.drop(['PassengerId'], axis=1)
 print(df)
 
 processed_df = preprocess_df(df)
-test_input = processed_df.values[:,1:]
-actual_output = processed_df.values[:,1]
+test_input = processed_df.values
+#actual_output = processed_df.values[:,1]
 
-predicted_output = model.predict(test_input).astype(int)
-df_output = pd.DataFrame()
+predicted_output = np.round(model.predict(test_input)).astype(int)
+df1['Survived'] = predicted_output
+df1['Survived'] = df1['Survived'].map(lambda s: 1 if s >= 0.5 else 0)
+print(df1)
+df1[['PassengerId','Survived']].to_csv('./Dataset/My_Results.csv', index=False)
+#df_output = pd.DataFrame()
